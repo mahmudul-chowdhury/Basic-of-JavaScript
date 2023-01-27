80b6e97d802a8ba282b3399b048e980392a810e5
mahmudul hasan chowdhury <mahmudul.chowdhury@tuni.fi>
2023-01-28 01:32:14 +0200

    Milestone 4.

---
+++ math.js
@@ -0,0 +1,18 @@
+/**
+ * 1. number ==> 55, 5665, -32
+ * 2. integer, odd, even
+ * 3. float: , decimal: 2.5, 13.67,
+ * 4. operation: addition, substraction, multiplication, division, remainder
+ * 5. percent
+ * 6. average
+ * 7. comparison: >, <, ==, !=, >=, <=
+ * 8, oikik: 2 kilo apple  and 5 kilo mango:
+ * 9. 45, 25, 56, 65, 129, 2: max, min, average, sum
+ * 10. series
+ * :::  EXTRA   :::
+ * 1. prime
+ * 2. permutation
+ * 3. area
+ * :::  ALWAYS  :::
+ * 
+ * **/
---
+++ variable-name.js
@@ -0,0 +1,12 @@
+//1. Perfect variable
+var price = 29;
+
+//2. Variable name cannot be any keywords. {ekhane var ekta keyword}
+var false = 98;
+var return = true;
+
+//2. Variable name has to be in one word. No space
+var my home address = "New California";
+
+//3: Variable name cannot have quotation
+var myHomeAddress = "New California";
