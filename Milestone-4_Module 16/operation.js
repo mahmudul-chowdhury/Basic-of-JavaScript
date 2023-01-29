d0be976a1d508169feb2589420524cdf42d9a3d5
mahmudul hasan chowdhury <mahmudul.chowdhury@tuni.fi>
2023-01-29 23:35:53 +0200

    Folder Changed

---
+++ Milestone-4_Module 16/operation.js
@@ -0,0 +1,28 @@
+var onionPrice = 20;
+var eggPrice = 10;
+
+//console.log(onionPrice);
+//console.log(eggPrice);
+//console.log(onionPrice + eggPrice);
+
+var totalPrice = onionPrice + eggPrice;
+//console.log(totalPrice);
+
+var priceDifference = onionPrice - eggPrice;
+//console.log(priceDifference);
+
+/**::::::::::::::::::::::::::::::::::::::::::::::::::::::**/
+
+var orangePrice = 20;
+var quantity = 7;
+
+var totalCost = orangePrice * quantity;
+//console.log(totalCost);
+
+/**:::::::::::::::::::::::::::::::::::::::::::::::::::::::**/
+
+var money = 500;
+var worker = 10;
+
+var eachWorker = money / worker;
+console.log(eachWorker);
---
+++ Milestone-4_Module 16/shorthand.js
@@ -0,0 +1,40 @@
+// variable declaration
+var tomatoPrice = 25;
+// variable value update
+tomatoPrice = 30;
+
+// add something to the first variable value
+var price1 = 31;
+var price2 = 10;
+
+// set a new value
+// price1 = 35;
+
+// add something to the existing value
+price1 = price1 + 10;
+// We can also declar this line in a short form and it's called short hand.
+// price1 += 10;
+//console.log(price1)
+
+//price2 = price2 - 5;
+price2 -= 5;
+//console.log(price2);
+
+// Short hand: *=
+price2 *= 5;
+//console.log(price2);
+
+// Short hand: /=
+price1 /= 5;
+//console.log(price1);
+
+
+var age = 15;
+//age = age + 1;
+// ++ means adding 1 to the existing value
+age++;
+
+var love = 100;
+//love = love - 1;
+// You are deducting 1 from the existing value
+ love--;
